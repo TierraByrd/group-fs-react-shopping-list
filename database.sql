9707d7e1ab5ef2db6eb1d4ac02ba0f21752d31a3
Tierra Byrd <tierrabyrd1@gmail.com>
2024-05-30 11:19:46 -0500

    done

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "shoppinglist"
+(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"quantity" DECIMAL NOT NULL,
+	"unit" VARCHAR(20),
+	"purcharsed" BOOLEAN DEFAULT FALSE
+);
+INSERT INTO "shoppinglist" ("name", "quantity", "unit")
+VALUES ('Apple', '5', 'each'),
+('Milk', '2', 'Gallons');
